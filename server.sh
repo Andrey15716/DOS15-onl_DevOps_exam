dadfc21dbc194cf6f4bbf1ea89d727610c3afa87
Andrey15716 <52579943+Andrey15716@users.noreply.github.com>
2023-12-06 20:11:12 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -10,6 +10,6 @@ sudo systemctl start nginx
 sudo systemctl enable nginx
 
 mkdir -p /home/ec2-user/.ssh
-echo "ed25519 cpcwuDMcG+ZoH7LiXKBqWR+I4kskoMIkHs311QwLF3o=" >> /home/ec2-user/.ssh/authorized_keys
+echo "ed25519 " >> /home/ec2-user/.ssh/authorized_keys
 chmod 600 /home/ec2-user/.ssh/authorized_keys
 chown -R ec2-user:ec2-user /home/ec2-user/.ssh
