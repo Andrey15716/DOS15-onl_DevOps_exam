cf3d5d5882ece05fa8e9494055aca049960689e8
Andrey15716 <52579943+Andrey15716@users.noreply.github.com>
2023-12-06 20:09:49 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,8 +10,8 @@ terraform {
 #aws provider and keys
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAU4L6PJKVGIIMIY4P"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
 #vpc resource
