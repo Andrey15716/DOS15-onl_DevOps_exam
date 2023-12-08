48ec05a3ae17b7b797470708d9ba92380364129e
Andrey <antonenkoa.cniitu@gmail.com>
2023-12-08 22:14:50 +0300

    jenkins file updated

--- server.sh
+++ server.sh
@@ -3,8 +3,7 @@
 sudo apt update
 sudo apt install -y nginx
 sudo apt install -y git
-
+export LC_ALL=C.UTF-8
 echo "Hello World" | sudo tee /var/www/html/index.html
-
 sudo systemctl start nginx
 sudo systemctl enable nginx
--- sshConnect.sh
+++ sshConnect.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-export LC_ALL=C.UTF-8
-
 sudo curl -o /var/www/html/index.html https://raw.githubusercontent.com/Andrey15716/DOS15-onl_DevOps_exam/main/application/index.html
 sudo systemctl restart nginx
-
