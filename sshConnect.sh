e0e8c87a6709e6fcfdb2f8c0818aac82599ea097
Andrey <antonenkoa.cniitu@gmail.com>
2023-12-08 21:14:48 +0300

    jenkins file updated

--- sshConnect.sh
+++ sshConnect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo curl -o /var/www/html/index.html https://raw.githubusercontent.com/Andrey15716/DOS15-onl_DevOps_exam/main/application/index.html
+echo "Hello World from AWS!" | sudo tee /var/www/html/index.html
 
 sudo systemctl start nginx
 sudo systemctl enable nginx
