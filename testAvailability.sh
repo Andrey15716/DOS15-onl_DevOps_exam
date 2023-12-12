d3171e26569941804ea4b17a62d780727cd370e3
Andrey <antonenkoa.cniitu@gmail.com>
2023-12-12 13:29:51 +0300

    jenkins file updated

--- testAvailability.sh
+++ testAvailability.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-$ALB_ADDRESS = $1
+ALB_ADDRESS=$1
 response=$(wget --spider -S "$ALB_ADDRESS" 2>&1 | grep "HTTP/" | awk '{print $2}')
-Check if the HTTP response code is 200 OK
+# Check if the HTTP response code is 200 OK
 if [ "$response" = "200" ]; then
-echo "Website is accessible"
+  echo "Website is accessible"
 else
-echo "Website is not accessible. Response: $response"
-exit 1
+  echo "Website is not accessible. Response: $response"
+  exit 1
 fi
