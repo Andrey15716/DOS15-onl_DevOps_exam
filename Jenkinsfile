be63e22af8b366fc63df4056697701af8a04286e
Andrey <antonenkoa.cniitu@gmail.com>
2023-12-12 13:14:34 +0300

    jenkins file updated

--- Jenkinsfile
+++ Jenkinsfile
@@ -41,13 +41,9 @@ pipeline {
     stage('Test Website') {
       steps {
         script {
-          def url = "http://my-alb-36497665.us-east-1.elb.amazonaws.com/"
-          def response = sh(script: "wget --spider -S ${url} 2>&1", returnStdout: true).trim()
-          if (response.contains("200 OK")) {
-            echo "Website is accessible"
-          } else {
-            error "Website is not accessible. Response: ${response}"
-          }
+          sh '''
+            ./testAvailability.sh {$ALB_ADDRESS}
+          '''
         }
       }
     }
--- testAvailability.sh
+++ testAvailability.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-ALB_ADDRESS="http://my-alb-36497665.us-east-1.elb.amazonaws.com/"
-responseCode=$(curl -f -s -o /dev/null -w '%{http_code}' "$ALB_ADDRESS")
-
-if [[ "$responseCode" -ne 200 ]]; then
-  echo "ALB Address $ALB_ADDRESS responded with code: $responseCode"
-  exit 1
+$ALB_ADDRESS = $1
+response=$(wget --spider -S "$ALB_ADDRESS" 2>&1 | grep "HTTP/" | awk '{print $2}')
+Check if the HTTP response code is 200 OK
+if [ "$response" = "200" ]; then
+echo "Website is accessible"
+else
+echo "Website is not accessible. Response: $response"
+exit 1
 fi
