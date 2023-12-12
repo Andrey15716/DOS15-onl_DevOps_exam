d90ad8f5d9c7504af4cb2c24de854b2ac2f2c68d
Andrey <antonenkoa.cniitu@gmail.com>
2023-12-12 16:07:32 +0300

    jenkins file updated

--- server.sh
+++ server.sh
@@ -4,6 +4,13 @@ sudo apt update
 sudo apt install -y nginx
 sudo apt install -y git
 export LC_ALL=C.UTF-8
+sudo apt install -y openjdk-11-jdk
+sudo apt update
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 echo "Hello World" | sudo tee /var/www/html/index.html
 sudo systemctl start nginx
 sudo systemctl enable nginx
--- sshConnect.sh
+++ sshConnect.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-sudo apt install -y openjdk-11-jdk
-sudo apt update
+
+sudo fallocate -l 1G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
 if curl -f -s -o /dev/null https://raw.githubusercontent.com/Andrey15716/DOS15-onl_DevOps_exam/main/application/index.html; then
   sudo curl -o /var/www/html/index.html https://raw.githubusercontent.com/Andrey15716/DOS15-onl_DevOps_exam/main/application/index.html
   sudo systemctl restart nginx
