d17b084b5c521ff6bdb12360582497bb68ecf23b
Andrey <antonenkoa.cniitu@gmail.com>
2023-12-12 12:16:43 +0300

    jenkins file updated

--- testAvailability.sh
+++ testAvailability.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-status_code=$(curl --write-out %{http_code} --silent --output /dev/null "http://my-alb-36497665.us-east-1.elb.amazonaws.com11")
+curl http://my-alb-36497665.us-east-1.elb.amazonaws.com/   \
+--silent \
+--insecure \
+--request POST \
+--header "your-curl-may-want-a-header" \
+--data @my.input.file \
+--output site.output \
+--write-out %{http_code} \
+  > http.response.code 2> error.messages
+errorLevel=$?
+httpResponse=$(cat http.response.code)
 
-if [[ "$status_code" != 200 ]] ; then
-  echo "ALB Address responded with code: $responseCode"
+
+jq --raw-output 'keys | @csv' site.output | sed 's/"//g' > return.keys
+hasErrors=`grep --quiet --invert errors return.keys;echo $?`
+
+if [[ $errorLevel -gt 0 ]] || [[ $hasErrors -gt 0 ]] || [[ "$httpResponse" != "200" ]]; then
+  echo -e "Error POSTing http://my-alb-36497665.us-east-1.elb.amazonaws.com/ with input my.input (errorLevel $errorLevel, http response code $httpResponse)" >> error.messages
+  send_exit_message
   exit 1
 fi
