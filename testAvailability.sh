52d983f9e9c7d1c5cf70ef5be717374700e80cfc
Andrey <antonenkoa.cniitu@gmail.com>
2023-12-12 11:32:19 +0300

    jenkins file updated

--- testAvailability.sh
+++ testAvailability.sh
@@ -4,6 +4,6 @@ ALB_ADDRESS="$1"
 responseCode=$(curl -s -o /dev/null -w '%{http_code}' $ALB_ADDRESS)
 
 if [ "$responseCode" != "200" ]; then
-  echo "Code response of $ALB_ADDRESS is $responseCode"
+  echo "Code response is $responseCode"
   exit 1
 fi
