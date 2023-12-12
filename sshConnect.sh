aee4ecd20977f5f91b00d2af753b7235bb9cc8ec
Andrey <antonenkoa.cniitu@gmail.com>
2023-12-12 15:59:36 +0300

    jenkins file updated

--- sshConnect.sh
+++ sshConnect.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
-
-mkdir -p ~/.ssh
-touch ~/.ssh/config
-chmod 600 ~/.ssh/config
-
-{
-  echo "Host *"
-  echo "  StrictHostKeyChecking no"
-  echo "  UserKnownHostsFile /dev/null"
-} >> ~/.ssh/config
-
+sudo apt install -y openjdk-11-jdk
+sudo apt update
 if curl -f -s -o /dev/null https://raw.githubusercontent.com/Andrey15716/DOS15-onl_DevOps_exam/main/application/index.html; then
   sudo curl -o /var/www/html/index.html https://raw.githubusercontent.com/Andrey15716/DOS15-onl_DevOps_exam/main/application/index.html
   sudo systemctl restart nginx
