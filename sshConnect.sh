53bd7eb1f3140a9d84aaa39e3695dafc391b9e38
Andrey <antonenkoa.cniitu@gmail.com>
2023-12-12 12:07:56 +0300

    jenkins file updated

--- sshConnect.sh
+++ sshConnect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if curl -f -s -o /dev/null https://raw.githubusercontent.com/Andrey15716/DOS15-onl_DevOps_exam/main/application/index.html1; then
+if curl -f -s -o /dev/null https://raw.githubusercontent.com/Andrey15716/DOS15-onl_DevOps_exam/main/application/index.html; then
   sudo curl -o /var/www/html/index.html https://raw.githubusercontent.com/Andrey15716/DOS15-onl_DevOps_exam/main/application/index.html
   sudo systemctl restart nginx
 else
--- testAvailability.sh
+++ testAvailability.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-ALB_ADDRESS="http://my-alb-36497665.us-east-1.elb.amazonaws.com/"
-responseCode=$(curl -f -s -o /dev/null -w '%{http_code}' "$ALB_ADDRESS")
+status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://my-alb-36497665.us-east-1.elb.amazonaws.com/)
 
-if [[ "$responseCode" -ne 200 ]]; then
-  echo "ALB Address $ALB_ADDRESS responded with code: $responseCode"
+if [[ "$status_code" -ne 200 ]] ; then
+  echo "ALB Address responded with code: $responseCode"
   exit 1
 fi
