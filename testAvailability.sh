f9ddaa30158b3191911132080c488ded39e36d35
Andrey <antonenkoa.cniitu@gmail.com>
2023-12-12 12:09:52 +0300

    jenkins file updated

--- testAvailability.sh
+++ testAvailability.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://my-alb-36497665.us-east-1.elb.amazonaws.com/)
+status_code=$(curl --write-out %{http_code} --silent --output /dev/null http://my-alb-36497665.us-east-1.elb.amazonaws.com11/)
 
 if [[ "$status_code" -ne 200 ]] ; then
   echo "ALB Address responded with code: $responseCode"
