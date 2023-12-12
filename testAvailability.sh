89910d33c96afca9aac8c2b440290d9e32ee1678
Andrey <antonenkoa.cniitu@gmail.com>
2023-12-12 11:59:51 +0300

    jenkins file updated

--- testAvailability.sh
+++ testAvailability.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 ALB_ADDRESS="http://my-alb-36497665.us-east-1.elb.amazonaws.com11/"
-responseCode=$(curl -f -s -o /dev/null -w '%{http_code}' $ALB_ADDRESS)
+responseCode=$(curl -f -s -o /dev/null -w '%{http_code}' "$ALB_ADDRESS")
 
-if [ "$responseCode" != "200" ]; then
-  echo "Code response is $responseCode"
+if [[ "$responseCode" -ne 200 ]]; then
+  echo "ALB Address $ALB_ADDRESS responded with code: $responseCode"
   exit 1
 fi
