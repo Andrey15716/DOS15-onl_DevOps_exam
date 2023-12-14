3fa71e5400232951bf29a6a4913c15d9fcb80ffb
Andrey <antonenkoa.cniitu@gmail.com>
2023-12-14 12:58:11 +0300

    index file was updated

--- testAvailability.sh
+++ testAvailability.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 ALB_ADDRESS="http://my-alb-36497665.us-east-1.elb.amazonaws.com/"
 response=$(wget --spider -S "$ALB_ADDRESS" 2>&1 | grep "HTTP/" | awk '{print $2}')
+
 if [ "$response" = "200" ]; then
-  echo "Website is accessible"
+  sudo curl -o /var/www/html/index.html https://raw.githubusercontent.com/Andrey15716/DOS15-onl_DevOps_exam/main/application/index.html
+  sudo systemctl restart nginx
 else
-  echo "Website is not accessible. Response: $response"
+  echo "Приложение недоступно"
   exit 1
 fi
