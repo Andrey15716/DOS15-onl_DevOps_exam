136e01adcf87268532ebe8a4a4b87a562fed636c
Andrey <antonenkoa.cniitu@gmail.com>
2023-12-14 12:48:03 +0300

    index file was updated

--- sshConnect.sh
+++ sshConnect.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-if curl -f -s -o /dev/null https://raw.githubusercontent.com/Andrey15716/DOS15-onl_DevOps_exam/main/application/index.html; then
-  sudo curl -o /var/www/html/index.html https://raw.githubusercontent.com/Andrey15716/DOS15-onl_DevOps_exam/main/application/index.html
-  sudo systemctl restart nginx
-else
-  echo "Приложение недоступно"
-  exit 1
-fi
+sudo curl -o /var/www/html/index.html https://raw.githubusercontent.com/Andrey15716/DOS15-onl_DevOps_exam/main/application/index.html
+sudo systemctl restart nginx
