90c4961400a2c6e1ae316bbef4f514d16e614565
Andrey <antonenkoa.cniitu@gmail.com>
2023-12-14 13:07:07 +0300

    index file was updated

--- sshConnect.sh
+++ sshConnect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-response=$(wget --spider -S "https://raw.githubusercontent.com/Andrey15716/DOS15-onl_DevOps_exam/main/application/index.html1" 2>&1 | grep "HTTP/" | awk '{print $2}')
+response=$(wget --spider -S "https://raw.githubusercontent.com/Andrey15716/DOS15-onl_DevOps_exam/main/application/index.html" 2>&1 | grep "HTTP/" | awk '{print $2}')
 
 if [ "$response" = "200" ]; then
   sudo curl -o /var/www/html/index.html https://raw.githubusercontent.com/Andrey15716/DOS15-onl_DevOps_exam/main/application/index.html
